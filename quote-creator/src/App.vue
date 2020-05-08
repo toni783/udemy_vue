ddaa1b7ecdeda4985abec49ab10531022350cd29
toni783 <toni7858@gmail.com>
2020-05-08 10:48:33 -0500

    simple quote creator added

---
+++ quote-creator/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @onCreateQuote="addQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click in a quote to dele it !</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header.vue";
+export default {
+  data: function() {
+    return {
+      quotes: ["Just a Quote to see something"],
+      maxQuotes: 10
+    };
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  },
+  methods: {
+    addQuote(val) {
+      if (this.quotes.length >= this.maxQuotes) {
+        alert("Please delete Quotes to continue!!");
+        return;
+      }
+      this.quotes.push(val);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ quote-creator/src/components/Quote.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-lg-3">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.panel-body {
+  font-family: "Arizonia", cursive;
+  font-size: 24px;
+  color: blue;
+}
+.quote {
+  cursor: pointer;
+}
+.quote:hover {
+  background-color: rgb(248, 153, 153);
+}
+</style>
